c843e60246594f684f70c0878b7f4b818d6ea439
Leonardo Nunes <leonardovnun@gmail.com>
2019-11-06 16:14:24 -0300

    Resolvido o problema do aceitabilidade do Tkinter com outros formatos de imagens, porém esta não parece na label

--- V1.0/main.py
+++ V1.0/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import tkinter as tk
+import time
+import threading
 from PIL import Image, ImageTk
 
 class Program():
@@ -14,30 +16,36 @@ class Program():
         # frame 3 = botton-right
 
         root.configure(background="white")
+        self.atualizaGrafico()
+        # load1 = Image.open("images/ABEV3.png")
+        # load2 = Image.open("images/ITUB4.png")
+        # load3 = Image.open("images/MGLU3.png")
+        # load4 = Image.open("images/PETR4.png")
+        #
+        # render = ImageTk.PhotoImage(load1)
+        # graph1 = Label(root, image=render)
+        # graph1.image = render
+        # graph1.place(x=39, y=-25)
+        #
+        # # load2 = Image.open("images/ITUB4.png")
+        # render = ImageTk.PhotoImage(load2)
+        # graph1 = Label(root, image=render)
+        # graph1.image = render
+        # graph1.place(x=639, y=-25)
+        #
+        # # load3 = Image.open("images/MGLU3.png")
+        # render = ImageTk.PhotoImage(load3)
+        # graph1 = Label(root, image=render)
+        # graph1.image = render
+        # graph1.place(x=39, y=261)
+        #
+        # # load4 = Image.open("images/PETR4.png")
+        # render = ImageTk.PhotoImage(load4)
+        # graph1 = Label(root, image=render)
+        # graph1.image = render
+        # graph1.place(x=639, y=261)
 
-        load = Image.open("images/ABEV3.png")
-        render = ImageTk.PhotoImage(load)
-        graph1 = Label(root, image=render)
-        graph1.image = render
-        graph1.place(x=39, y=-25)
-
-        load = Image.open("images/ITUB4.png")
-        render = ImageTk.PhotoImage(load)
-        graph1 = Label(root, image=render)
-        graph1.image = render
-        graph1.place(x=639, y=-25)
-
-        load = Image.open("images/MGLU3.png")
-        render = ImageTk.PhotoImage(load)
-        graph1 = Label(root, image=render)
-        graph1.image = render
-        graph1.place(x=39, y=261)
-
-        load = Image.open("images/PETR4.png")
-        render = ImageTk.PhotoImage(load)
-        graph1 = Label(root, image=render)
-        graph1.image = render
-        graph1.place(x=639, y=261)
+            # time.sleep(15)
 
         metrABEV3 = tk.IntVar()
         metrMGLU3 = tk.IntVar()
@@ -54,21 +62,21 @@ class Program():
         tk.Radiobutton(root, text="Fechamento                  ", padx=0, variable=metrABEV3, value=4, bg="white").place(x=39, y=662)
         tk.Radiobutton(root, text="Sem indicador               ", padx=0, variable=metrABEV3, value=5,bg="white").place(x=39, y=684)
 
-
+        # metricas MGLU3
         tk.Radiobutton(root, text="Médias Móveis             ", padx=0, variable=metrMGLU3, value=1, bg="white").place(x=250, y=596)
         tk.Radiobutton(root, text="Volume                         ", padx=0, variable=metrMGLU3, value=2, bg="white").place(x=250, y=618)
         tk.Radiobutton(root, text="Banda de Bollinger       ", padx=0, variable=metrMGLU3, value=3, bg="white").place(x=250, y=640)
         tk.Radiobutton(root, text="Fechamento                  ", padx=0, variable=metrMGLU3, value=4, bg="white").place(x=250, y=662)
         tk.Radiobutton(root, text="Sem indicador               ", padx=0, variable=metrMGLU3, value=5,bg="white").place(x=250, y=684)
 
-
+        # metricas PETR4
         tk.Radiobutton(root, text="Médias Móveis             ", padx=0, variable=metrPETR4, value=1, bg="white").place(x=461,y=596)
         tk.Radiobutton(root, text="Volume                         ", padx=0, variable=metrPETR4, value=2, bg="white").place(x=461, y=618)
         tk.Radiobutton(root, text="Banda de Bollinger       ", padx=0, variable=metrPETR4, value=3, bg="white").place(x=461,y=640)
         tk.Radiobutton(root, text="Fechamento                  ", padx=0, variable=metrPETR4, value=4, bg="white").place(x=461,y=662)
         tk.Radiobutton(root, text="Sem indicador               ", padx=0, variable=metrPETR4, value=5,bg="white").place(x=461, y=684)
 
-
+        # metricas ITUB4
         tk.Radiobutton(root, text="Médias Móveis             ", padx=0, variable=metrITUB4, value=1, bg="white").place(x=672,y=596)
         tk.Radiobutton(root, text="Volume                         ", padx=0, variable=metrITUB4, value=2, bg="white").place(x=672, y=618)
         tk.Radiobutton(root, text="Banda de Bollinger       ", padx=0, variable=metrITUB4, value=3, bg="white").place(x=672,y=640)
@@ -87,12 +95,57 @@ class Program():
         buyITUB4 = tk.Button(root, text="COMPRAR ITUB4", width=15, height=0).place(x=1050, y=650)
         sellITUB4 = tk.Button(root, text="VENDER ITUB4", width=15, height=0).place(x=1050, y=684)
 
-        # self.photo = PhotoImage(file="images/c0books_read.jpeg")
-        # self.backlabel = Label(self.frame0, image=self.photo).place(x=-1, y=-1)
 
+    def atualizaGrafico(self):
+        load = Image.open("images/ABEV3.png")
+        render = ImageTk.PhotoImage(load)
+        graph1 = Label(root, image=render)
+        graph1.image = render
+        graph1.place(x=39, y=-25)
 
+        load2 = Image.open("images/ITUB4.png")
+        render = ImageTk.PhotoImage(load2)
+        graph1 = Label(root, image=render)
+        graph1.image = render
+        graph1.place(x=639, y=-25)
 
+        load3 = Image.open("images/MGLU3.png")
+        render = ImageTk.PhotoImage(load3)
+        graph1 = Label(root, image=render)
+        graph1.image = render
+        graph1.place(x=39, y=261)
 
-root = tk.Tk()
-Program(root)
-root.mainloop()
+        load4 = Image.open("images/PETR4.png")
+        render = ImageTk.PhotoImage(load4)
+        graph1 = Label(root, image=render)
+        graph1.image = render
+        graph1.place(x=639, y=261)
+
+        def bolsolindo(self):
+            print("MORAUM 2022")
+
+def iniciar():
+
+    print("1")
+    root = tk.Tk()
+    print("2")
+    Program(root)
+    print("3")
+    root.mainloop()
+
+# def biroliro():
+#     global root
+#     while True:
+#         if root != None:
+#             print('oi8')
+#
+#             root.atualizaGrafico()
+#             print('7oi')
+#
+#         time.sleep(10)
+#         print('oi')
+
+thread1 = threading.Thread(target=iniciar)
+thread1.start()
+# thread2 = threading.Thread(target=biroliro)
+# thread2.start()
